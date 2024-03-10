1b36e93d115339fd7b0003cf1669a06fcba45b87
Istrate Andrei <andrei_istrate20@yahoo.ro>
2024-03-10 23:05:39 +0200

    First 3 tasks

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -5,6 +5,9 @@
 #include "common.h"
 #include <opencv2/core/utils/logger.hpp>
 #include <string>
+#include <iostream>
+
+using namespace std;
 
 #define path "E:\\Scoala\\Facultate\\An 3\\PI\\flower_images";
 
@@ -13,8 +16,12 @@ void openImagesBatch()
 	std::string folderPath = path;
 
 	// Array of flower folder names
-	std::string flowerFolders[] = { "Lilly", "Lotus", "Orchid", "Sunflower", "Tulip" };
-
+	// 0 - Lilly, 1 - Lotus, 2 - Orchid, 3 - Sunflower, 4 - Tulip
+	std::string flowerFolders[] = {"Lilly", "Lotus", "Orchid", "Sunflower", "Tulip"};
+	std::vector<cv::String> train;
+	std::vector<cv::String> test;
+	map<String, int> flowersMap;
+	int i = 0;
 	for (const auto& flowerFolder : flowerFolders)
 	{
 		std::string currentFolderPath = folderPath + "\\" + flowerFolder;
@@ -24,20 +31,31 @@ void openImagesBatch()
 		cv::glob(currentFolderPath + "\\*.jpg", imageFiles, false);
 
 		// Iterate through the found files
+		int counter = 0;
 		for (const auto& imagePath : imageFiles)
 		{
+			flowersMap.insert(pair<String, int>(imagePath, i));
 			cv::Mat src = cv::imread(imagePath);
-
+			if (counter == 0)
+				train.push_back(imagePath);
+			else 
+				test.push_back(imagePath);
 			if (!src.data)
 			{
 				std::cerr << "Error loading image: " << imagePath << std::endl;
 				continue;
 			}
-
-			imshow("image", src);
-			waitKey();
+			counter = 1 - counter;
 		}
+		i++;
 	}
+	/*map<string, int>::iterator it = flowersMap.begin();
+	while (it != flowersMap.end()) {
+		cout << "Key: " << it->first
+			<< ", Value: " << it->second << endl;
+		++it;
+	}*/
+
 }
 
 int main() 
@@ -45,72 +63,5 @@ int main()
 
 	openImagesBatch();
 
-	/*
-	cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_FATAL);
-    projectPath = _wgetcwd(0, 0);
-
-	int op;
-	do
-	{
-		system("cls");
-		destroyAllWindows();
-		printf("Menu:\n");
-		printf(" 1 - Open image\n");
-		printf(" 2 - Open BMP images from folder\n");
-		printf(" 3 - Image negative\n");
-		printf(" 4 - Image negative (fast)\n");
-		printf(" 5 - BGR->Gray\n");
-		printf(" 6 - BGR->Gray (fast, save result to disk) \n");
-		printf(" 7 - BGR->HSV\n");
-		printf(" 8 - Resize image\n");
-		printf(" 9 - Canny edge detection\n");
-		printf(" 10 - Edges in a video sequence\n");
-		printf(" 11 - Snap frame from live video\n");
-		printf(" 12 - Mouse callback demo\n");
-		printf(" 0 - Exit\n\n");
-		printf("Option: ");
-		scanf("%d",&op);
-		switch (op)
-		{
-			case 1:
-				testOpenImage();
-				break;
-			case 2:
-				testOpenImagesFld();
-				break;
-			case 3:
-				testNegativeImage();
-				break;
-			case 4:
-				testNegativeImageFast();
-				break;
-			case 5:
-				testColor2Gray();
-				break;
-			case 6:
-				testImageOpenAndSave();
-				break;
-			case 7:
-				testBGR2HSV();
-				break;
-			case 8:
-				testResize();
-				break;
-			case 9:
-				testCanny();
-				break;
-			case 10:
-				testVideoSequence();
-				break;
-			case 11:
-				testSnap();
-				break;
-			case 12:
-				testMouseClick();
-				break;
-		}
-	}
-	while (op!=0);
 	return 0;
-	*/
 }
